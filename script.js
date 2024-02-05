ed5b1bf9a93fc53f59cd795d66c4957bf90fbb80
Dunja <dunjabeki@gmail.com>
2024-02-05 12:16:35 +0100

    Toggle cart

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Open and close cart */
+
+const cartIcon = document.querySelector('#cart-icon');
+const cart = document.querySelector('.cart');
+const closeCart = document.querySelector('#cart-close');
+
+cartIcon.addEventListener('click', () => {
+  cart.classList.add('active')
+})
+
+closeCart.addEventListener('click', () => {
+  cart.classList.remove('active')
+})
+
