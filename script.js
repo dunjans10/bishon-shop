1df3716ef919d23df5b30a57ad281d74dd8ae129
Dunja <dunjabeki@gmail.com>
2024-02-06 10:42:14 +0100

    Adding hero section and marquee section

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//Marquee
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+} 
+
+
 /* Open and close cart */
 
 const cartIcon = document.querySelector('#cart-icon');
